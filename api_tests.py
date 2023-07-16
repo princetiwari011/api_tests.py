5e3b77bb657c03a2220bb125225a1b3c36f0406c
Anil Tiwari <aniltiwari@smcfinance.com>
2023-07-16 13:53:53 +0530

    This is update new for UMS API test_api_request

--- api_tests.py
+++ api_tests.py
@@ -1,44 +1,101 @@
+import pytest
 import json
 import requests
-import pytest
 
 base_URL = "http://ums.devopsmcfinance.com/umsnode/ums/api/v1"
 auth_endpoint = "/auth/login"
-
+AccessToken="/access-token"
 login_url = base_URL + auth_endpoint
-payload = {
-    "userCode": "M3084",
-    "userPass": "M3084",
-    "application": ["User Management System"]
-}
+payload = {"userCode": "M3084","userPass": "M3084","application": ["User Management System"]}
 headers = {"Content-Type": "application/json"}
-
 # Perform login and extract the refresh token
 response = requests.post(login_url, json=payload, headers=headers)
 response_data = response.json()
-refresh_token = response_data['data']['accessToken']
+print(f"Endpoint: {auth_endpoint}")
+print(f"Status Code: {response.status_code}")
+refreshToken = response_data['data']['refreshToken']
+login_url = base_URL +AccessToken
+payload = {refreshToken}
+response_data = response.json()
+accessToken = response_data['data']['accessToken']
+print(f"Endpoint: {AccessToken}")
+print(f"Status Code: {response.status_code}")
 
-ENDPOINT_LIST = [
-    "/master/users", "/master/users", "/lookup/dashbaoard", "/master/application", "/master/branch",
-    "/master/department", "/master/designation", "/master/department", "/master/department/1", "/master/product-type",
-    "/master/product-type", "/master/product", "/master/product-type", "/master/product", "/master/scheme",
-    "/master/product", "/master/program", "/master/product", "/master/menuevent", "/master/module",
-    "/master/application", "/master/menu", "/master/application", "/master/group", "/master/role", "/master/users-v2",
-    "/master/department", "/master/designation", "/master/employee", "/master/department", "/master/designation",
-    "/user-mapping/program-scheme", "/master/program", "/master/scheme", "/user-mapping/branch-application",
-    "/master/branch", "/master/application", "/master/menu", "/master/application", "/master/module",
-    "/master/menuevent", "/user-mapping/branch-scheme", "/master/branch", "/master/scheme", "/master/menu",
-    "/master/application", "/master/module", "/master/menuevent", "/user-mapping/menu-module-event/menu-modules/103",
-    "/user-mapping/menu-module-event/module-events/58", "/user-mapping/group-event", "/master/group",
-    "/master/application", "/user-mapping/menu-module-event/menu-event", "/user-mapping/role-group", "/master/role",
-    "/master/group", "/user-mapping/user-branch", "/master/users", "/master/branch", "/user-mapping/user-product",
-    "/master/users", "/master/product", "/user-mapping/user-application", "/master/users", "/master/application",
-    "/user-mapping/user-role", "/master/users", "/master/role"
-]
-
-for endpoint in ENDPOINT_LIST:
+# Test case for fetching APIs
+@pytest.mark.parametrize("endpoint", [
+    "/master/users",
+    "/master/users",
+    "/lookup/dashbaoard",
+    "/master/application",
+    "/master/branch",
+    "/master/department/",
+    "/master/designation/",
+    "/master/department/",
+    "/master/department/",
+    "/master/product-type",
+    "/master/product-type",
+    "/master/product",
+    "/master/product-type",
+    "/master/product",
+    "/master/scheme",
+    "/master/product",
+    "/master/program",
+    "/master/product",
+    "/master/menuevent",
+    "/master/module",
+    "/master/application",
+    "/master/menu",
+    "/master/application",
+    "/master/group",
+    "/master/role",
+    "/master/users-v2",
+    "/master/department/",
+    "/master/designation",
+    "/master/employee",
+    "/master/department/",
+    "/master/designation",
+    "/user-mapping/program-scheme",
+    "/master/program",
+    "/master/scheme",
+    "/user-mapping/branch-application",
+    "/master/branch",
+    "/master/application",
+    "/master/menu",
+    "/master/application",
+    "/master/module",
+    "/master/menuevent",
+    "/user-mapping/branch-scheme",
+    "/master/branch",
+    "/master/scheme",
+    "/master/menu",
+    "/master/application",
+    "/master/module",
+    "/master/menuevent",
+    "/user-mapping/menu-module-event/menu-modules/103",
+    "/user-mapping/menu-module-event/module-events/58",
+    "/user-mapping/group-event",
+    "/master/group",
+    "/master/application",
+    "/user-mapping/menu-module-event/menu-event",
+    "/user-mapping/role-group",
+    "/master/role",
+    "/master/group",
+    "/user-mapping/user-branch",
+    "/master/users",
+    "/master/branch",
+    "/user-mapping/user-product",
+    "/master/users",
+    "/master/product",
+    "/user-mapping/user-application",
+    "/master/users",
+    "/master/application",
+    "/user-mapping/user-role",
+    "/master/users",
+    "/master/role"
+])
+def test_api_request(endpoint):
     full_url = base_URL + endpoint
-    headers = {"Authorization": "Bearer " + refresh_token}
+    headers = {"Authorization": "Bearer " + accessToken}
 
     try:
         if endpoint == "/lookup/dashbaoard":
@@ -57,19 +114,17 @@ for endpoint in ENDPOINT_LIST:
         print(f"Endpoint: {endpoint}")
         print(f"Status Code: {response.status_code}")
         # print(f"Response: {response_data}")
-        print()
-
-        # Perform assertions or validations as needed
-        # Example:
-        # assert 'data' in response_data
-        # assert isinstance(response_data['data'], list)
+        # print()
     except requests.exceptions.RequestException as e:
         print(f"Error occurred for endpoint: {endpoint}")
         print(f"Error message: {str(e)}")
         # print()
 
     if response.status_code == 200:
-            print("Hi Anil")
+        print("This API has been run successfully 😊")
     else:
-            print("Not fetched. Please try again.")
+        print("Not fetched. Please try again.🥲😔😔")
 
+# Execute the tests
+if __name__ == '__main__':
+    pytest.main()
